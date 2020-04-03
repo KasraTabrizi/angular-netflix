c86e465ceddc4c2ca1e5c66beb3e3966792a33ce
KasraTabrizi <kasra_tabrizi@hotmail.com>
2020-04-03 15:45:47 +0200

    added necessary functions and reading url param

--- src/app/cast-details/cast-details.component.ts
+++ src/app/cast-details/cast-details.component.ts
@@ -8,12 +8,28 @@ import { ActivatedRoute } from '@angular/router'
   styleUrls: ['./cast-details.component.scss']
 })
 export class CastDetailsComponent implements OnInit {
+  id: String;
+  private sub: any;
+  detailsCast: Object;
+  detailsCredit: Object;
 
   constructor(private _http: HttpService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.sub = this.route.params.subscribe(params => {
+      this.id = params['id'];
+      console.log(this.id);
+    });
+
+    this._http.getDetailsOfCast(this.id).subscribe(data => {
+      this.detailsCast = data;
+      console.log(this.detailsCast);
+    });
+
+    this._http.getCreditsOfCast(this.id).subscribe(data => {
+      this.detailsCredit = data;
+      console.log(this.detailsCredit);
+    });
+
   }
-
-
-
 }
