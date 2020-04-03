419208ac52282c369b6abbd41839a41d6f26ecf1
KasraTabrizi <kasra_tabrizi@hotmail.com>
2020-04-03 09:49:34 +0200

    custom pipe for converting minutes to hh:mm format

--- src/app/my-time.pipe.ts
+++ src/app/my-time.pipe.ts
@@ -5,8 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MyTimePipe implements PipeTransform {
 
-  transform(value: String): String {
-    return value + '1';
-  }
+  transform(value: number): String {
+    let hour = 0;
+    let time = 0;
+    let minutes = 0;
 
+    time = value / 60;
+    hour = Math.floor(time);
+    minutes = Math.floor((time - hour) * 60);
+
+    return String(hour) + "h " + String(minutes) + "m";
+  }
 }
