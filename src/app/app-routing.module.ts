7bdaa20c0dbd82cbff15232c91b1a8d053a8deb0
KasraTabrizi <kasra_tabrizi@hotmail.com>
2020-04-03 13:41:10 +0200

    added searchDetailsComponent route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { SearchDetailsComponent } from './search-details/search-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'movie-details/:id', component: MovieDetailsComponent }
+  { path: 'search-details/:query', component: SearchDetailsComponent }
   // { path: '**', component: PageNotFoundComponent }
 ];
 
